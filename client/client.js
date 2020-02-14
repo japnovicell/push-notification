2df37a284a6fd37f67a03f35becd27d0a69e7eeb
jannick pedersen <jap@novicell.dk>
2020-02-14 09:28:27 +0100

    apple test

--- client/client.js
+++ client/client.js
@@ -17,10 +17,14 @@ console.log('Service Worker Registered...');
 
 //Register Push
 console.log('Registering Push...');
+
+if(registration.pushManager){
 const subscription = await register.pushManager.subscribe({
 userVisibleOnly: true,
 applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
 });
+
+
 console.log('Push Registered...');
 
   // Send Push Notification
@@ -32,6 +36,39 @@ console.log('Push Registered...');
 console.log('Push Sent...');
 
 }
+else{
+  var button = document.getElementsByTagName("button");
+button.onclick = function() {
+    // Ensure that the user can receive Safari Push Notifications.
+    if ('safari' in window && 'pushNotification' in window.safari) {
+        var permissionData = window.safari.pushNotification.permission('web.com.example.domain');
+        checkRemotePermission(permissionData);
+    }
+};
+
+var checkRemotePermission = function (permissionData) {
+    if (permissionData.permission === 'default') {
+        // This is a new web service URL and its validity is unknown.
+        window.safari.pushNotification.requestPermission(
+            'https://domain.example.com', // The web service URL.
+            'web.com.example.domain',     // The Website Push ID.
+            {}, // Data that you choose to send to your server to help you identify the user.
+            checkRemotePermission         // The callback function.
+        );
+    }
+    else if (permissionData.permission === 'denied') {
+        // The user said no.
+    }
+    else if (permissionData.permission === 'granted') {
+        // The web service URL is a valid push provider, and the user said yes.
+        // permissionData.deviceToken is now available to use.
+    }
+};
+
+
+}
+}
+
 
 function urlBase64ToUint8Array(base64String) {
     const padding = '='.repeat((4 - base64String.length % 4) % 4);
