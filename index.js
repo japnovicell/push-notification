8db6cf457a8cd056ed6c97f605e8c3bfa12bee23
jannick pedersen <jap@novicell.dk>
2020-02-14 08:46:02 +0100

    change

--- index.js
+++ index.js
@@ -37,6 +37,6 @@ app.post('/subscribe', (reg, res) => {
    .catch(err => console.error(err));
 });
 
-const port = process.env.port  || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
