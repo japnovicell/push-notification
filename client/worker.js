3289d21367c2980fe4fe14008fa5bf9b664fed8a
jannick pedersen <jap@novicell.dk>
2020-02-17 13:41:00 +0100

    back

--- client/worker.js
+++ client/worker.js
@@ -8,24 +8,3 @@ self.addEventListener ('push', e=> {
         icon:'https://cdn.shopify.com/s/files/1/0036/4806/1509/products/s0105131_cd5ce0b3-2852-45e6-b98d-3cfc0b238501.jpg?v=1578563558'
     });
 });
-
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
-  '/',
-  'index.html',
-  'client.js',
-  'manifest.json',
-  '/index.js'
-];
-
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
